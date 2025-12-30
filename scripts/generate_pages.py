c6c900ebc4da1e48ca3e5f309c9bf68ebaa3a273
jesperkha <hammerjesp@gmail.com>
2025-12-30 13:12:11 +0100

    Hardcoded date

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -5,17 +5,33 @@ import markdown_code_blocks
 
 DEV = False
 
+
 def template(source: str, name: str, insert: str) -> str:
-    return source.replace("{"+name+"}", insert) 
+    return source.replace("{" + name + "}", insert)
+
 
 def filename_to_title(filename: str) -> str:
-    return " ".join(filename.removesuffix(".md").capitalize().split("-"))
+    split = filename.split("_")
+    name = split[0] if len(split) == 1 else split[1]
+    return " ".join(name.removesuffix(".md").capitalize().split("-"))
+
+
+def filename_to_date(filename: str) -> str:
+    split = filename.split("_")
+    if len(split) > 1:
+        return f"({split[0]})"
+
+    return ""
+
 
 def replace_image_template(html):
     pattern = r"<p>\{IMAGE ([^}]+)\}</p>"
-    if DEV: replaced_html = re.sub(pattern, r'<img src="../assets/\1">', html)
-    else: replaced_html = re.sub(pattern, r'<img src="/curiosities/assets/\1">', html)
+    if DEV:
+        replaced_html = re.sub(pattern, r'<img src="../assets/\1">', html)
+    else:
+        replaced_html = re.sub(pattern, r'<img src="/curiosities/assets/\1">', html)
     return replaced_html
+
 
 def page_md_to_html(filename: str, out_dir: str, template_file: str):
     with open(f"pages/{filename}", "r") as f:
@@ -30,11 +46,10 @@ def page_md_to_html(filename: str, out_dir: str, template_file: str):
                 if file == "index.md":
                     continue
 
-                file_link = f"[{filename_to_title(file)}](src/{file})"
-                mod_unix_time = os.path.getmtime(f"pages/{file}")
-                dt_object = datetime.fromtimestamp(mod_unix_time)
-                modified = dt_object.strftime('%d.%m.%y')
-                entries += f"- ({modified}) {file_link}\n"
+                file_link = (
+                    f"{filename_to_date(file)} [{filename_to_title(file)}](src/{file})"
+                )
+                entries += f"- {file_link}\n"
 
             md = template(md, "ENTRIES", entries)
 
@@ -47,18 +62,21 @@ def page_md_to_html(filename: str, out_dir: str, template_file: str):
             page = template(page, "BODY", html)
             wf.write(page)
 
+
 def create_pages(source_dir: str, out_dir: str):
     files = os.listdir(source_dir)
     template_file = open("templates/page.html", "r")
     template_str = template_file.read()
     for page in files:
         page_md_to_html(page, out_dir, template_str)
-    
+
     template_file.close()
+
 
 def create_index():
     with open("templates/index.html", "r") as templ:
         page_md_to_html("index.md", ".", templ.read())
+
 
 if __name__ == "__main__":
     create_pages("pages", "src")
